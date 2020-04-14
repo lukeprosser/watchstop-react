a8f08e379a9c1b8b557c7d6ab1339915529fe556
Luke Prosser <luke.prosser@cloud-iq.com>
2020-04-14 21:04:33 +0100

    Add container to header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,21 +8,23 @@ export default class Navbar extends Component {
   render() {
     return (
       <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
-        <ul className="navbar-nav align-items-center">
-          <li>
-            <Link to="/" className="navbar-brand text-uppercase">
-              WatchStop
-            </Link>
-          </li>
-        </ul>
-        <Link to="/cart" className="ml-auto">
-          <ButtonContainer>
-            <span className="mr-2">
-              <i className="fas fa-cart-plus" />
-            </span>
-            Cart
-          </ButtonContainer>
-        </Link>
+        <div className="container">
+          <ul className="navbar-nav align-items-center">
+            <li>
+              <Link to="/" className="navbar-brand text-uppercase">
+                WatchStop
+              </Link>
+            </li>
+          </ul>
+          <Link to="/cart" className="ml-auto">
+            <ButtonContainer>
+              <span className="mr-2">
+                <i className="fas fa-cart-plus" />
+              </span>
+              Cart
+            </ButtonContainer>
+          </Link>
+        </div>
       </NavWrapper>
     )
   }
